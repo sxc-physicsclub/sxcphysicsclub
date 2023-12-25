f82ac7a33cd0a8f862b76ef45873cf56f11abd00
thearjunneupane <arjnep000@gmail.com>
2023-12-25 15:30:49 +0545

    Updated Files

--- public/js/functions.js
+++ public/js/functions.js
@@ -47,4 +47,37 @@ export function initializeTabs(activeClass, ...tabsData) {
     // Show default tab contents when the page loads
     showDefaultTabContent();
   }
+
+  export function initializeVideoPopup() {
+    // Get elements
+    const videoPlayBtns = document.querySelectorAll(".video-play-btn");
+    const videoPopup = document.querySelector(".video-popup");
+    const videoPopupCloseBtn = document.querySelector(".video-popup-close");
+    const videoIframe = videoPopup.querySelector(".player");
+  
+    function toggleVideoPopup(videoSrc) {
+      if (videoPopup.classList.contains("open") && videoIframe.src === videoSrc) {
+        videoPopup.classList.remove("open");
+        videoIframe.src = "";
+      } else {
+        videoPopup.classList.add("open");
+        if (videoSrc) {
+          videoIframe.src = videoSrc;
+        }
+      }
+    }
+  
+    // Add click event handlers
+    videoPlayBtns.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        const videoSrc = this.getAttribute("data-video-url");
+        toggleVideoPopup(videoSrc);
+      });
+    });
+  
+    videoPopupCloseBtn.addEventListener("click", function () {
+      videoPopup.classList.remove("open");
+      videoIframe.src = "";
+    });
+  }
   
